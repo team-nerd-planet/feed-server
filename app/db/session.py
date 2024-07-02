e1e4cc274751142ff53c4b8986bc286dbb1fa9e0
thewoowon <thewoowon@naver.com>
2024-07-03 08:02:28 +0900

    feat: resolve conflict from main

--- app/db/session.py
+++ app/db/session.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgresql+asyncpg://postgre:planetnerd1!@pg-ffp3k.vpc-pub-cdb-kr.ntruss.com:5432/postgres'
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
